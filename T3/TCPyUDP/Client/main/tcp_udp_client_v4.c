c24c5496a4ab0adc4afa1d0053968bf045278acb
Álvaro Tobar <ankaph@protonmail.com>
2023-07-12 19:38:55 -0400

    renombrado tcp_client a tcp_udp_client y agregada la parte de udp

--- T3/TCPyUDP/Client/main/tcp_udp_client_v4.c
+++ T3/TCPyUDP/Client/main/tcp_udp_client_v4.c
@@ -30,7 +30,79 @@
 static const char *TAG = "example";
 
 void udp_client(char protocol) {
-    return;
+    while (1) {
+        int addr_family = 0;
+        int ip_protocol = 0;
+#if defined(CONFIG_EXAMPLE_IPV4)
+        struct sockaddr_in dest_addr;
+        dest_addr.sin_addr.s_addr = inet_addr(HOST_IP_ADDR);
+        dest_addr.sin_family = AF_INET;
+        dest_addr.sin_port = htons(PORT + 4);
+        addr_family = AF_INET;
+        ip_protocol = IPPROTO_IP;
+#elif defined(CONFIG_EXAMPLE_SOCKET_IP_INPUT_STDIN)
+        struct sockaddr_storage dest_addr = { 0 };
+        ESP_ERROR_CHECK(get_addr_from_stdin(PORT + 4, SOCK_DGRAM, &ip_protocol, &addr_family, &dest_addr));
+#endif
+        int sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
+        if (sock < 0) {
+            ESP_LOGE(TAG, "Unable to create socket UDP: errno %d", errno);
+            break;
+        }
+                // Set timeout
+        struct timeval timeout;
+        timeout.tv_sec = 10;
+        timeout.tv_usec = 0;
+        setsockopt (sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof timeout);
+
+        ESP_LOGI(TAG, "Socket created, sending to %s:%d", HOST_IP_ADDR, PORT + 4);
+
+        while (1) {
+            char* payload = malloc(messageLength(protocol));
+            char* header = malloc(12);
+            char* message;
+
+            int int_protocol = (int)protocol - 48;
+
+            switch (int_protocol) {
+                case 1:
+                    message = dataprotocol1(header);
+                    break;
+                case 2:
+                    message = dataprotocol2(header);
+                    break;
+                case 3:
+                    message = dataprotocol3(header);
+                    break;
+                case 4:
+                    message = dataprotocol4(header);
+                    break;
+                case 5:
+                    message = dataprotocol4(header);
+                    break;
+                default:
+                    message = dataprotocol1(header);
+                    break;
+            }
+            memcpy((void*)&(payload[0]), (void*)header, 12);
+            memcpy((void*)&(payload[12]), (void*)message, dataLength(int_protocol));
+            free(header);
+            free(message);
+
+
+            int err = sendto(sock, payload, messageLength(int_protocol), 0, (struct sockaddr *)&dest_addr, sizeof(dest_addr)); //setea direccion de destino
+            free(payload);
+
+            if (err < 0) {
+                ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+                break;
+            }
+            ESP_LOGI(TAG, "Message sent");
+
+            vTaskDelay(2000 / portTICK_PERIOD_MS); // ayuda para problemas de conexión. Limpiar variables en el puerto en el que se recibe con un print
+        }
+    }
+
 }
 
 void tcp_client(char protocol) {
